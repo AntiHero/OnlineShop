d7e5e2590e8968f858899fb31cab3afb8d4ab75f
AntiHero <prophreaker@gmail.com>
2021-06-03 14:56:16 +0000

    Deploying to gh-pages from  @ 01a9502aa108af39813bb992e87141a7bc490c89 🚀

--- js/index.js
+++ js/index.js
@@ -1,14 +1,32 @@
+function tabClickHandler(e) {
+  e.preventDefault();
+
+  $('.search__tabs-item').removeClass('search__tabs-item--active');
+  $('.search__content-item').removeClass('search__content-item--active');
+
+  $(this).addClass('search__tabs-item--active');
+
+  $($(this).attr('href')).addClass('search__content-item--active');
+}
+
 $(() => {
   $('.banner-section__slider').slick({
     dots: true,
-    prevArrow: `<button 
-    class="banner-section__slider-slick-btn banner-section__slider-slick-btn--prev">
-    <img src="images/arrow-left.svg" alt="">
-    </button>`,
-    nextArrow: `<button 
-    class="banner-section__slider-slick-btn banner-section__slider-slick-btn--next">
-    <img src="images/arrow-right.svg" alt="">
+    prevArrow: `
+    <button 
+      class="banner-section__slider-slick-btn banner-section__slider-slick-btn--prev"
+    >
+      <img src="images/arrow-left.svg" alt="">
+    </button>
+    `,
+    nextArrow: `
+    <button 
+      class="banner-section__slider-slick-btn banner-section__slider-slick-btn--next"
+    >
+      <img src="images/arrow-right.svg" alt="">
     </button>
     `,
   });
+
+  $('.search__tabs-item').on('click', tabClickHandler);
 });
